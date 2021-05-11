57e2cd1e493c86cd4e9204977cdd4b0b84594d5e
Eli Pacheco <eli.pacheco55@gmail.com>
2021-05-11 11:30:42 -0600

    more practice

--- eli_pacheco_resume.cpp
+++ eli_pacheco_resume.cpp
@@ -5,10 +5,16 @@
 #include <fstream>
 using namespace std;
 
+
+void thisfunction(){
+  cout << "This is me remembering that c++ is more difficult than python" << endl;
+
+}
 int main () {
   ofstream myfile;
   myfile.open ("example.txt");
   myfile << "This is a practice string" << endl;
   myfile.close();
+  thisfunction();
   return 0;
 }
